facc585ae03080358c8f7d4c4679078e9430b5f5
ahanifraka <ahanifraka@gmail.com>
2025-07-09 19:33:27 +0700

    update

--- register.php
+++ register.php
@@ -79,11 +79,11 @@
                     </div>
                     <div>
                         <label class="block text-xs font-semibold mb-1" for="phone">
-                            Nomor Agen
+                            Nama Agen
                         </label>
                         <input
                             class="w-full border border-gray-300 rounded-md px-3 py-2 text-sm placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-[#009eff]"
-                            id="phone" name="phone" placeholder="Masukkan No Handphone" type="tel" />
+                            id="name" name="name" placeholder="Masukkan Nama Agen / (PT)" type="tel" />
                     </div>
                     <div>
                         <label class="block text-xs font-semibold mb-1" for="pin">
